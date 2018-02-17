8087f68cb690c59a8f5b0089090d2fc601bdae5b
Kevin Ventura <venutra93@gmail.com>
2018-02-16 21:58:52 -0600

    Getting started

---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+    </head>
+    <body>
+        Hola, este es el <?php echo $title; ?>
+        La url es: <?php echo $url; ?>
+    </body>
